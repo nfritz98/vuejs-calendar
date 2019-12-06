8fd854bea8134abae07e7ed844b1a00598e5c68e
Nicole Fritz <n.fritz@netz98.de>
2019-12-06 14:03:28 +0100

    Video 129 group days in weeks

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div v-for="day in days">{{ day }}</div>
+        <div v-for="week in weeks">
+            Week
+            <div v-for="day in week">{{ day }}</div>
+        </div>
     </div>
 </template>
 
@@ -56,6 +59,22 @@
                }
 
                return days;
+           },
+           weeks(){
+               //multidimensional array
+               let weeks = [];
+               let week = [];
+
+               //group all days in separate weeks
+               for( let day of this.days){
+                   week.push(day);
+                   if(week.length === 7){
+                       weeks.push(week);
+                       week =[];
+                   }
+               }
+
+               return weeks;
            }
        }
    }
