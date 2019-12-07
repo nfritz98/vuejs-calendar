7eab2c8bc8e9c60817d7dacdf9eb99aacbeb6563
Nicole Fritz <n.fritz@netz98.de>
2019-12-07 13:46:07 +0100

    Video 133 highlight current day

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="day"> {{ day.format('D') }}</div>
+    <div :class="classObject" > {{ day.format('D') }}</div>
 </template>
 
 <script>
     export default {
-        props: ['day']
+        props: ['day'],
+        computed: {
+            classObject(){
+                return {
+                    day: true,
+                    today: this.day.isSame(this.$moment(), 'day')
+                }
+            }
+        }
     }
 </script>
