ba2865f79923b8a7a978031be4bf2da3ffc4a2eb
Nicole Fritz <n.fritz@netz98.de>
2019-12-08 11:35:41 +0100

    Video 137 merge video 17-branch, add currentMonth

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -6,12 +6,12 @@
         props: [ 'day' ],
         computed: {
             classObject() {
-                let today = this.day.isSame(this.$moment(), 'day');
+                let isToday = this.day.isSame(this.$moment(), 'day');
                 return {
                     day: true,
-                    today,
-                    past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
-                };
+                    today: isToday,
+                    past: this.day.isSameOrBefore(this.$moment(), 'day') && !isToday
+                }
             }
         }
     }
