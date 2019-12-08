7269c1ff6358449a371e27400bbb263c6b8ed3b5
Nicole Fritz <n.fritz@netz98.de>
2019-12-08 11:38:28 +0100

    Video 138 output current formatted month + year

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,12 +1,19 @@
 <template>
-
+    <div>{{ formattedDate }}</div>
 </template>
 <script>
     export default {
         props: [ 'month' ],
         computed: {
-
-
+            formattedDate(){
+                return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+            },
+            month() {
+                return this.$store.state.currentMonth;
+            },
+            year() {
+                return this.$store.state.currentYear;
+            }
         }
     }
 </script>
