d9843b83ea43833c96d91a2a09642895de1e5c80
Nicole Fritz <n.fritz@netz98.de>
2019-12-08 14:22:51 +0100

    Video 146, change eventForm-position on click

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,8 +1,17 @@
 <template>
-    <div id="event-form" :class="{active: true }" :style="{top: '300px', left: '500px'}"></div>
+    <div id="event-form" :class="{active: true }" :style="{top: top, left: left}"></div>
 </template>
 
 <script>
-
+    export default {
+        computed: {
+            top(){
+                return `${this.$store.state.eventFormPositionY}px`;
+            },
+            left(){
+                return `${this.$store.state.eventFormPositionX}px`;
+            }
+        }
+    }
 
 </script>
