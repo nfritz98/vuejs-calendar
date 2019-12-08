a32f6d0eb5898110980211e199fdf06b084eaa7a
Nicole Fritz <n.fritz@netz98.de>
2019-12-08 11:40:24 +0100

    Video 139 add click-fucntionality to month-buttons

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>{{ formattedDate }}</div>
+    <div>
+        <div>{{ formattedDate }}</div>
+        <button @click="decrease">-</button>
+        <button @click="increase">+</button>
+    </div>
 </template>
 <script>
     export default {
         props: [ 'month' ],
+        methods: {
+            decrease(){
+console.log('-');
+            },
+            increase(){
+                console.log('+');
+
+            },
+        },
         computed: {
             formattedDate(){
                 return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
